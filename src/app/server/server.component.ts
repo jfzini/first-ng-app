ad0858f67723d99726b351b704055849aab87226
João Felipe Zini <joaofelipezini@gmail.com>
2023-07-07 10:32:17 -0300

    learning about styleUrls

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
+  styleUrls: ['../servers/servers.component.css'],
 })
 export class ServerComponent {
 
