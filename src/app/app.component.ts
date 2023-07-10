a0d43013a3a027c69e1b81dfddd7e463d61e09de
João Felipe Zini <joaofelipezini@gmail.com>
2023-07-10 12:45:22 -0300

    ng directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  name = 'João Felipe Zini';
-  age = '29';
+export class AppComponent implements OnInit {
+
+  constructor() {
+    type User = {stringOutput: string};
+  }
+
+  name: string = 'João Felipe Zini';
+  age: number = 28;
+  increaseAge: boolean = false;
+  showAge: boolean = false;
+  userData = {
+    stringOutput: '',
+  }
+
+  onIncreaseAge() {
+    this.age++;
+    this.showAge = true;
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.increaseAge = true;
+    }, 3000);
+  }
 }
